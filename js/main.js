661fd2037c74e8d5229d64b0353d8f30238afd18
sowely <pikulinadasha99@gmail.com>
2021-07-12 23:46:53 +0700

    gridssss, js

--- js/main.js
+++ js/main.js
@@ -1,39 +1,25 @@
 // scroll to UP to DOWN
 const btnUp = document.querySelector('.footer__btn-up')
 const btnDown = document.querySelector('.btn-down')
+const header = document.querySelector('.header')
 const blockPrice = document.getElementById('price')
-// console.log(blockPrice)
-let scrolled
-let timer
 
-btnUp.addEventListener('click', () => {
-	// window.scrollTo(0,0)
-	scrolled = window.pageYOffset
-	scrollUp()
+btnUp.addEventListener('click', (e) => {
+	e.preventDefault();
+	header.scrollIntoView({ behavior: 'smooth' });
 })
 
-btnDown.addEventListener('click', () => {
+btnDown.addEventListener('click', (e) => {
+	e.preventDefault();
 	blockPrice.scrollIntoView({ behavior: 'smooth', block: 'center' });
 })
-
-function scrollUp() {
-	if (scrolled > 0) {
-		window.scrollTo(0, scrolled)
-		scrolled = scrolled - 100
-		timer = setTimeout(scrollUp, 10)
-	}
-	else {
-		clearTimeout(timer)
-		window.scrollTo(0, 0)
-	}
-}
 
 
 // price toggle month/year checked
 const btnMonth = document.querySelector('.info__btn-month')
 const btnYear = document.querySelector('.info__btn-year')
-const priceMonthYear = document.querySelector('.card__price')
-// console.log(priceMonthYear)
+const priceMonthYear = document.querySelectorAll('.card__price span')
+
 btnMonth.addEventListener('click', priceToggle)
 btnYear.addEventListener('click', priceToggle)
 
@@ -45,13 +31,21 @@ function priceToggle(e) {
 			target.classList.add('checked')
 			btnMonth.classList.remove('checked')
 			// priceMonthYear.innerHTML = 'asda123'
+			priceMonthYear.forEach(el => {
+				el.innerHTML = (el.innerHTML * 12).toFixed(2);
+			});
 		}
 		else {
 			target.classList.add('checked')
 			btnYear.classList.remove('checked')
+			priceMonthYear.forEach(el => {
+				el.innerHTML = (el.innerHTML / 12).toFixed(2);
+			});
 		}
 	}
 }
+
+
 
 // subscribe
 const emailSub = document.querySelector('.subscribe__input-btn')
